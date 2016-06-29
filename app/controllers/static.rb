a843a128b10e698087ffbe9182febfe31f9307d4
nextacademy <dteohyl@gmail.com>
2016-06-29 14:53:56 +0800

    Done with HTML functions. Beginning CSS

--- app/controllers/static.rb
+++ app/controllers/static.rb
@@ -1,15 +1,24 @@
 require 'byebug'
 get '/' do
 	@urls = Url.all
-  erb :"static/index"
+	@errors = params[:error_msg]
+  	erb :"static/index"
 end
 
 
 post '/urls' do
 	@urls = Url.all
-	@url = Url.new(url: params[:long_url], short_url: Url.shorten)
-	@url.save
-	redirect '/'
+	@url = Url.new(url: params[:long_url])
+	# byebug
+	if @url.save #it will go to app/models/url.rb go thru all the validation tests
+	
+		redirect '/'
+
+	else
+		@errors = "#{@url.errors.full_messages.join(",")}"# this will display error message that u inserted in ur model validation test
+		# erb :"static/index"
+		redirect "/?error_msg=#{@errors}"
+	end
 end
 
 get '/:short_url' do 
@@ -18,6 +27,10 @@ get '/:short_url' do
 	@url.increment(:click_count)
 	@url.save  	
 
-	redirect "http://" + @url.url
+		if @url.url.match(/http[s]?/) == nil
+			redirect "http://" + @url.url
 
+		else
+			redirect @url.url
+		end   
 end 
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,16 +1,20 @@
 class Url < ActiveRecord::Base
 
-	validates :url , uniqueness:true 
-	validate :valid_url
+	validates :url , uniqueness:{ case_sensitive: false}
+	validates_format_of :url, with: /(http[s]?.*|w{3}.*)/, message: "format is invalid"
+	# before_create :shorten
+	before_create :shorten
 
-	def self.shorten
-		SecureRandom.hex(3)
+	def shorten
+		self.short_url = SecureRandom.hex(3)
 	end 
 
-	def valid_url
-		if self.url.match(/(http[s]?.*|w{3}.*)/) == nil
-			errors.add(:url, "Invalid url!")
-		end 
-	end 
+	# def valid_url
+	# 
+	# 	if self.url.match(/(http[s]?.*|w{3}.*)/) == nil
+	# 		errors.add(:url, "Invalid url!")
+	# 		self.url = false 
+	# 	end 
+	# end 
 
 end
