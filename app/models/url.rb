5be283496d1deba6a142b74ed6d1b5a085dafa52
nextacademy <dteohyl@gmail.com>
2016-06-30 14:39:29 +0800

    As good as it'll get for now (: acceptable level of performance!

--- app/models/url.rb
+++ app/models/url.rb
@@ -3,14 +3,14 @@ class Url < ActiveRecord::Base
 	validates :url , uniqueness:{ case_sensitive: false}
 	validates_format_of :url, with: /(http[s]?.*|w{3}[.].*)/, message: "format is invalid"
 	# before_create :shorten
-	before_create :shorten
+	before_create :shorten, :valid_url
 
 	def shorten
 		self.short_url = SecureRandom.hex(3)
 	end 
 
 	# def valid_url
-	# 
+	
 	# 	if self.url.match(/(http[s]?.*|w{3}.*)/) == nil
 	# 		errors.add(:url, "Invalid url!")
 	# 		self.url = false 
